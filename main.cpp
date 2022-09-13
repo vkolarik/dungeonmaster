f7c9099f9cde55b1f470f462856151e4b17d5c4e
xkolari1 <xkolari1@mendelu.cz>
2022-09-13 07:51:31 +0200

    Init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by xkolari1 on 13.09.2022.
+//
+
+//můj první soubor
+
+#include "main.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by xkolari1 on 13.09.2022.
+//
+
+#ifndef ZOOZS2022_XKOLARI1_MAIN_H
+#define ZOOZS2022_XKOLARI1_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ZOOZS2022_XKOLARI1_MAIN_H
