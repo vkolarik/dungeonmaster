ca16f28bc5e9e7829276ed1ff5cdfc9e7e9b8e5c
Vojtěch Kolařík <xkolari1@mendelu.cz>
2022-09-27 08:02:54 +0200

    Naprogramovana fixa

--- c3/main.cpp
+++ c3/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
 class Fixa {
-public:
+private:
     std::string m_barva;
     float m_stav_n;
+public:
+    Fixa(std::string barva) {
+        m_barva = barva;
+        m_stav_n = 100;
+    };
 
-    void pis(){
+    void pis() {
+        if (m_stav_n > 0) {
+            std::cout << "Jsem fixa a pisu " + m_barva << std::endl;
+            m_stav_n -= 10;
+        } else {
+            std::cout << "Dosla napln" << std::endl;
+        }
 
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Fixa *fixa1 = new Fixa("zelena");
+    fixa1->pis();
+
+    for (int i = 0; i < 15; ++i) {
+        std::cout << "pokus" << i << std::endl;
+        fixa1->pis();
+    }
+
     return 0;
 }
